88e73fdd5d228e54d64b128611f1500d178f4397
npappassl <npappas@scottlogic.com>
2017-05-18 09:56:40 +0100

    RESTORE POINT -- before reducers

--- app/store.js
+++ app/store.js
@@ -7,6 +7,7 @@ import { fromJS } from 'immutable';
 import { routerMiddleware } from 'react-router-redux';
 import createSagaMiddleware from 'redux-saga';
 import createReducer from './reducers';
+import homePageReducer from ''
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -40,7 +41,9 @@ export default function configureStore(initialState = {}, history) {
 
   // Extensions
   store.runSaga = sagaMiddleware.run;
-  store.asyncReducers = {}; // Async reducer registry
+  store.asyncReducers = {
+
+  }; // Async reducer registry
 
   // Make reducers hot reloadable, see http://mxs.is/googmo
   /* istanbul ignore next */
